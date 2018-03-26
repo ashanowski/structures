5e0e5fd264ce97055725ebeca9d21ea0955203e8
ashanowski <ashanowski@cock.li>
2018-03-26 19:53:47 +0200

    linked_list.py updated

--- linked_list.py
+++ linked_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.6
 from node import Node
+import numpy as np
 
 
 class LinkedList:
-
     """ Linked list class, made of nodes """
+
     def __init__(self, head=None):
         self.len = 1
         self.comparisons = 0
@@ -13,8 +14,10 @@ class LinkedList:
         self.tail = self.head
 
     def show_counters(self):
-        """ Show counters of how many comparisons
-            and assigns were made
+        """
+        Show counters of how many comparisons
+        and assigns were made
+
         """
         print('Comparisons made: ', self.comparisons)
         print('Assigns made: ', self.assigns)
@@ -30,7 +33,11 @@ class LinkedList:
         """ Prints every element from the linked list """
 
         current = self.head
+        if current is None:
+            self.comparisons +=1
+            raise ValueError("List is empty")
         i = 1
+        self.assigns += 2
         while current.get_value() is not None:
             print('{}:'.format(i), current.get_value())
             i += 1
@@ -40,22 +47,37 @@ class LinkedList:
                 current = current.get_next()
 
     def append(self, value):
-        """ Takes a value, creates a node with that value
-            and attaches it to the list, placing at head
-            pointing new node at old head."""
+        """
+        Takes a value, creates a node with that value
+        and attaches it to the tail of the list
 
-        new_node = Node(value, None)
-        new_node.set_next(self.head)
-        self.head = new_node
-        self.len += 1
-        self.assigns += 5
+        """
+
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            current = self.head
+            while current.get_next() is not None:
+                current = current.get_next()
+            current.set_next(Node(value))
 
     def insert(self, value):
+        """
+        Inserts a value at the beginning of the list,
+        attaching it to the head
 
+        """
+
+        new_node = Node(value)
+        new_node.set_next(self.head)
+        self.head = new_node
 
     def size(self):
-        """ Counts nodes as long as they exist, increasing
-            count by 1 each time it founds it. Returns count"""
+        """
+        Counts nodes as long as they exist, increasing
+        count by 1 each time it founds it. Returns Counts
+
+        """
 
         current = self.head
         count = 0
@@ -65,20 +87,19 @@ class LinkedList:
         return count
 
     def search(self, value):
-        """ Search list for the value
-            and return the node containing it
+        """
+        Search list for the value
+        and return the node containing it
 
         """
 
         current = self.head
         found = False
-
         while current and found is False:
             if current.get_value() == value:
                 found = True
             else:
                 current = current.get_next()
-
         if current is None:
             raise ValueError("Given value not in a list")
         return current
@@ -101,3 +122,37 @@ class LinkedList:
             self.head = current.get_next()
         else:
             previous.set_next(current.get_next())
+
+
+np.random.seed(100)
+def fill_by_appending(_list):
+    """
+    Fills the list with 25 random integers
+    from [0, 300] range
+
+    """
+
+    for _ in range(25):
+        _list.append(np.random.randint(0, 301))
+    return _list
+
+def fill_mixed(_list):
+    """
+    Fills the list once with append, then with insert """
+    while _list.len < 25:
+        _list.append(np.random.randint(0, 301))
+        if _list.len == 25:
+            break
+        else:
+            _list.insert(np.random.randint(0, 301))
+
+def fill_random(_list):
+    """ Fills the list randomly, either at start, or beggining """
+
+    while _list.len < 25:
+        rand = np.random.random_sample(1)
+        if rand < 0.5:
+            _list.append(np.random.randint(0, 301))
+        else:
+            _list.insert(np.random.randint(0, 301))
+    return _list
